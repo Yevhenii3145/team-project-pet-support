ce57145dd92cda82e042864121251fa5d433c178
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2023-01-28 15:36:22 +0200

    Создал общую структуру папок, установил несколько библиотек  28.01

---
+++ src/UserRoutes.jsx
@@ -0,0 +1,4 @@
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+// import PrivateRoute from "components/PrivateRoute/PrivateRoute";
+// import PublicRoute from "components/PublicRoute/PublicRoute";
