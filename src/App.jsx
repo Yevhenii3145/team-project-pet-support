b94e88deb68a71ca4cbf113088b895e9e2391f57
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2023-01-28 21:25:35 +0200

    исправил Апп 28.01

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,7 @@ export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Navigate to="sales" />} />
-          <Route path="dashboard" element={<div>Dashboard</div>} />
-          <Route path="sales" element={<Sales />}>
-            <Route index element={<div>Sales index route</div>} />
-            <Route path="analytics" element={<div>Analytics</div>} />
-            <Route path="invoices" element={<Invoices />}>
-              <Route index element={<div>Invoices index route</div>} />
-              <Route path=":invoiceId" element={<InvoiceDetails />} />
-            </Route>
-            <Route path="deposits" element={<div>Deposits</div>} />
-          </Route>
-          <Route path="reports" element={<div>Reports</div>} />
-          <Route path="feedback" element={<div>Feedback</div>} />
-          <Route path="customers" element={<Customers />} />
-          <Route path="customers/:customerId" element={<CustomerDetails />} />
-        </Route>
       </Routes>
-      <GlobalStyle />
-      <Toaster position="top-right" reverseOrder={false} />
     </>
   );
 };
