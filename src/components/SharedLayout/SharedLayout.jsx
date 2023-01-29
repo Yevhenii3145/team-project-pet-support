307af682021d7fd0862ed467e8bbaad4a2ca5b19
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2023-01-30 01:14:46 +0200

    еще немного правок с шередлаяут

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,9 +1,16 @@
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
 import scss from "./shared-layout.module.scss";
 
 const SharedLayout = () => {
 
     return (
-        <div>SharedLayout</div>
+        <>
+            <div>SharedLayout</div>
+            <Suspense>
+                <Outlet />
+            </Suspense>
+        </>
     )
 }
 
