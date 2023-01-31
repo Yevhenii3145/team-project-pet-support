68ee06cacf914a723409743e1434202e190de84e
Sarat862 <sarat862@gmail.com>
2023-01-31 21:46:57 +0200

    Add file

--- src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -1,9 +1,33 @@
 import scss from "./notice-category-item.module.scss";
+import noticeImage from "images/dog1.webp";
+import heartSvg from "images/symbol-defs.svg#icon-edit";
 
 const NoticeCategoryItem = () => {
 
     return (
-        <div>NoticeCategoryItem</div>
+        <ul className={scss.card_list}>
+            <li className={scss.card_item}>
+                <img src={noticeImage} alt="pet" width="280" />
+                <div className={scss.card_info}>
+                    <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
+                    <ul className={scss.card_info_list}>
+                        <li className={scss.card_info_item}>Breed: Pomeranian</li>
+                        <li className={scss.card_info_item}>Place: Lviv</li>
+                        <li className={scss.card_info_item}>Age: one year</li>
+                        <li className={scss.card_info_item}>Price: 50$</li>
+                    </ul>
+                    <button type="button" className={scss.add_to_favorite_btn}>
+                        <svg width="24" height="22">
+                            <use href={heartSvg}></use>
+                        </svg>
+                    </button>
+                    <button type="button" className={scss.learn_more_btn}>Learn more</button>
+                    <p className={scss.card_text}>Sell</p>
+                </div>
+            </li>
+
+            
+        </ul>
     )
 }
 
--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,9 +1,19 @@
 import scss from "./notices-page.module.scss";
+import container from "helpers/container.module.scss";
+
+import NoticesCategoriesNav from "components/NoticesCategoriesNav/NoticesCategoriesNav";
+import NoticeCategoryItem from "components/NoticeCategoryItem/NoticeCategoryItem";
 
 const NoticesPage = () => {
 
     return (
-        <div>NoticesPage</div>
+        <main className={scss.main}>
+            <div className={container.container}>
+                <h2 className ={scss.main_title}>Find your favorite pet</h2>
+                <NoticesCategoriesNav />
+                <NoticeCategoryItem />
+            </div>
+        </main>
     )
 }
 
