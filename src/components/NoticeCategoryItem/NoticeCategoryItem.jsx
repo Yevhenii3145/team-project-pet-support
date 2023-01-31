7d83006eb53092b8384b7b85d9a08a8eacb9d61b
Sarat862 <sarat862@gmail.com>
2023-02-01 00:41:19 +0200

    Зробив верстку мобільної версії сторінки NoticesPage

--- src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -1,13 +1,16 @@
 import scss from "./notice-category-item.module.scss";
-import noticeImage from "images/dog1.webp";
-import heartSvg from "images/symbol-defs.svg#icon-edit";
+import noticeImage1 from "images/dog1.webp";
+import noticeImage2 from "images/dog2.webp"
+import noticeImage3 from "images/dog3.webp"
+import SvgInsert from "components/Svg/Svg";
+
 
 const NoticeCategoryItem = () => {
 
     return (
         <ul className={scss.card_list}>
             <li className={scss.card_item}>
-                <img src={noticeImage} alt="pet" width="280" />
+                <img src={noticeImage1} alt="pet" width="280" />
                 <div className={scss.card_info}>
                     <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
                     <ul className={scss.card_info_list}>
@@ -17,16 +20,138 @@ const NoticeCategoryItem = () => {
                         <li className={scss.card_info_item}>Price: 50$</li>
                     </ul>
                     <button type="button" className={scss.add_to_favorite_btn}>
-                        <svg width="24" height="22">
-                            <use href={heartSvg}></use>
-                        </svg>
+                        <SvgInsert id="icon-heart"/>
                     </button>
                     <button type="button" className={scss.learn_more_btn}>Learn more</button>
                     <p className={scss.card_text}>Sell</p>
                 </div>
             </li>
 
-            
+            <li className={scss.card_item}>
+                <img src={noticeImage2} alt="pet" width="280" />
+                <div className={scss.card_info}>
+                    <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
+                    <ul className={scss.card_info_list}>
+                        <li className={scss.card_info_item}>Breed: Pomeranian</li>
+                        <li className={scss.card_info_item}>Place: Lviv</li>
+                        <li className={scss.card_info_item}>Age: one year</li>
+                        <li className={scss.card_info_item}>Price: 50$</li>
+                    </ul>
+                    <button type="button" className={scss.add_to_favorite_btn}>
+                        <SvgInsert id="icon-heart"/>
+                    </button>
+                    <button type="button" className={scss.learn_more_btn}>Learn more</button>
+                    <p className={scss.card_text}>Sell</p>
+                </div>
+            </li>
+
+            <li className={scss.card_item}>
+                <img src={noticeImage3} alt="pet" width="280" />
+                <div className={scss.card_info}>
+                    <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
+                    <ul className={scss.card_info_list}>
+                        <li className={scss.card_info_item}>Breed: Pomeranian</li>
+                        <li className={scss.card_info_item}>Place: Lviv</li>
+                        <li className={scss.card_info_item}>Age: one year</li>
+                        <li className={scss.card_info_item}>Price: 50$</li>
+                    </ul>
+                    <button type="button" className={scss.add_to_favorite_btn}>
+                        <SvgInsert id="icon-heart"/>
+                    </button>
+                    <button type="button" className={scss.learn_more_btn}>Learn more</button>
+                    <p className={scss.card_text}>Sell</p>
+                </div>
+            </li>
+
+            <li className={scss.card_item}>
+                <img src={noticeImage1} alt="pet" width="280" />
+                <div className={scss.card_info}>
+                    <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
+                    <ul className={scss.card_info_list}>
+                        <li className={scss.card_info_item}>Breed: Pomeranian</li>
+                        <li className={scss.card_info_item}>Place: Lviv</li>
+                        <li className={scss.card_info_item}>Age: one year</li>
+                        <li className={scss.card_info_item}>Price: 50$</li>
+                    </ul>
+                    <button type="button" className={scss.add_to_favorite_btn}>
+                        <SvgInsert id="icon-heart"/>
+                    </button>
+                    <button type="button" className={scss.learn_more_btn}>Learn more</button>
+                    <p className={scss.card_text}>Sell</p>
+                </div>
+            </li>
+
+            <li className={scss.card_item}>
+                <img src={noticeImage2} alt="pet" width="280" />
+                <div className={scss.card_info}>
+                    <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
+                    <ul className={scss.card_info_list}>
+                        <li className={scss.card_info_item}>Breed: Pomeranian</li>
+                        <li className={scss.card_info_item}>Place: Lviv</li>
+                        <li className={scss.card_info_item}>Age: one year</li>
+                        <li className={scss.card_info_item}>Price: 50$</li>
+                    </ul>
+                    <button type="button" className={scss.add_to_favorite_btn}>
+                        <SvgInsert id="icon-heart"/>
+                    </button>
+                    <button type="button" className={scss.learn_more_btn}>Learn more</button>
+                    <p className={scss.card_text}>Sell</p>
+                </div>
+            </li>
+
+            <li className={scss.card_item}>
+                <img src={noticeImage3} alt="pet" width="280" />
+                <div className={scss.card_info}>
+                    <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
+                    <ul className={scss.card_info_list}>
+                        <li className={scss.card_info_item}>Breed: Pomeranian</li>
+                        <li className={scss.card_info_item}>Place: Lviv</li>
+                        <li className={scss.card_info_item}>Age: one year</li>
+                        <li className={scss.card_info_item}>Price: 50$</li>
+                    </ul>
+                    <button type="button" className={scss.add_to_favorite_btn}>
+                        <SvgInsert id="icon-heart"/>
+                    </button>
+                    <button type="button" className={scss.learn_more_btn}>Learn more</button>
+                    <p className={scss.card_text}>Sell</p>
+                </div>
+            </li>
+
+            <li className={scss.card_item}>
+                <img src={noticeImage1} alt="pet" width="280" />
+                <div className={scss.card_info}>
+                    <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
+                    <ul className={scss.card_info_list}>
+                        <li className={scss.card_info_item}>Breed: Pomeranian</li>
+                        <li className={scss.card_info_item}>Place: Lviv</li>
+                        <li className={scss.card_info_item}>Age: one year</li>
+                        <li className={scss.card_info_item}>Price: 50$</li>
+                    </ul>
+                    <button type="button" className={scss.add_to_favorite_btn}>
+                        <SvgInsert id="icon-heart"/>
+                    </button>
+                    <button type="button" className={scss.learn_more_btn}>Learn more</button>
+                    <p className={scss.card_text}>Sell</p>
+                </div>
+            </li>
+
+            <li className={scss.card_item}>
+                <img src={noticeImage2} alt="pet" width="280" />
+                <div className={scss.card_info}>
+                    <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
+                    <ul className={scss.card_info_list}>
+                        <li className={scss.card_info_item}>Breed: Pomeranian</li>
+                        <li className={scss.card_info_item}>Place: Lviv</li>
+                        <li className={scss.card_info_item}>Age: one year</li>
+                        <li className={scss.card_info_item}>Price: 50$</li>
+                    </ul>
+                    <button type="button" className={scss.add_to_favorite_btn}>
+                        <SvgInsert id="icon-heart"/>
+                    </button>
+                    <button type="button" className={scss.learn_more_btn}>Learn more</button>
+                    <p className={scss.card_text}>Sell</p>
+                </div>
+            </li>
         </ul>
     )
 }
