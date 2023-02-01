0877638659c7b29f9366b5950f6a21321255062b
Sarat862 <sarat862@gmail.com>
2023-02-01 11:46:28 +0200

    Зробив верстку для планшета

--- src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -10,7 +10,25 @@ const NoticeCategoryItem = () => {
     return (
         <ul className={scss.card_list}>
             <li className={scss.card_item}>
-                <img src={noticeImage1} alt="pet" width="280" />
+                <img src={noticeImage1} alt="pet" className={scss.card_img} />
+                <div className={scss.card_info}>
+                    <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
+                    <ul className={scss.card_info_list}>
+                        <li className={scss.card_info_item}>Breed: Pomeranian</li>
+                        <li className={scss.card_info_item}>Place: Lviv</li>
+                        <li className={scss.card_info_item}>Age: one year</li>
+                        <li className={scss.card_info_item}>Price: 50$</li>
+                    </ul>
+                    <button type="button" className={scss.add_to_favorite_btn}>
+                        <SvgInsert id="icon-heart"/>
+                    </button>
+                    <button type="button" className={scss.learn_more_btn} >Learn more</button>
+                    <p className={scss.card_text}>Sell</p>
+                </div>
+            </li>
+
+            <li className={scss.card_item}>
+                <img src={noticeImage2} alt="pet" className={scss.card_img} />
                 <div className={scss.card_info}>
                     <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
                     <ul className={scss.card_info_list}>
@@ -28,7 +46,7 @@ const NoticeCategoryItem = () => {
             </li>
 
             <li className={scss.card_item}>
-                <img src={noticeImage2} alt="pet" width="280" />
+                <img src={noticeImage3} alt="pet" className={scss.card_img} />
                 <div className={scss.card_info}>
                     <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
                     <ul className={scss.card_info_list}>
@@ -46,7 +64,7 @@ const NoticeCategoryItem = () => {
             </li>
 
             <li className={scss.card_item}>
-                <img src={noticeImage3} alt="pet" width="280" />
+                <img src={noticeImage1} alt="pet" className={scss.card_img} />
                 <div className={scss.card_info}>
                     <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
                     <ul className={scss.card_info_list}>
@@ -64,7 +82,7 @@ const NoticeCategoryItem = () => {
             </li>
 
             <li className={scss.card_item}>
-                <img src={noticeImage1} alt="pet" width="280" />
+                <img src={noticeImage2} alt="pet" className={scss.card_img} />
                 <div className={scss.card_info}>
                     <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
                     <ul className={scss.card_info_list}>
@@ -82,7 +100,7 @@ const NoticeCategoryItem = () => {
             </li>
 
             <li className={scss.card_item}>
-                <img src={noticeImage2} alt="pet" width="280" />
+                <img src={noticeImage3} alt="pet" className={scss.card_img} />
                 <div className={scss.card_info}>
                     <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
                     <ul className={scss.card_info_list}>
@@ -100,7 +118,7 @@ const NoticeCategoryItem = () => {
             </li>
 
             <li className={scss.card_item}>
-                <img src={noticeImage3} alt="pet" width="280" />
+                <img src={noticeImage1} alt="pet" className={scss.card_img} />
                 <div className={scss.card_info}>
                     <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
                     <ul className={scss.card_info_list}>
@@ -118,25 +136,7 @@ const NoticeCategoryItem = () => {
             </li>
 
             <li className={scss.card_item}>
-                <img src={noticeImage1} alt="pet" width="280" />
-                <div className={scss.card_info}>
-                    <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
-                    <ul className={scss.card_info_list}>
-                        <li className={scss.card_info_item}>Breed: Pomeranian</li>
-                        <li className={scss.card_info_item}>Place: Lviv</li>
-                        <li className={scss.card_info_item}>Age: one year</li>
-                        <li className={scss.card_info_item}>Price: 50$</li>
-                    </ul>
-                    <button type="button" className={scss.add_to_favorite_btn}>
-                        <SvgInsert id="icon-heart"/>
-                    </button>
-                    <button type="button" className={scss.learn_more_btn}>Learn more</button>
-                    <p className={scss.card_text}>Sell</p>
-                </div>
-            </li>
-
-            <li className={scss.card_item}>
-                <img src={noticeImage2} alt="pet" width="280" />
+                <img src={noticeImage2} alt="pet" className={scss.card_img} />
                 <div className={scss.card_info}>
                     <h3 className={scss.card_info_title}>Сute dog looking for a home</h3>
                     <ul className={scss.card_info_list}>
