3edb32ada20ebfbdf4cec7d1385a43e5bf66a6ef
Sarat862 <sarat862@gmail.com>
2023-02-01 19:17:28 +0200

    Зробив верстку планшетної і десктопної версії сторінки NoticesPage

--- src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
+++ src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
@@ -5,13 +5,13 @@ const NoticesCategoriesNav = () => {
 
     return (
         <ul className={scss.list}>
-            <li className={scss.tiem}>
+            <li className={scss.item}>
                 <Link to="lost-found" className={scss.link}>lost/found</Link>
             </li>
-            <li className={scss.tiem}>
+            <li className={scss.item}>
                 <Link to="for-free" className={scss.link}>in good hands</Link>
             </li>
-            <li className={scss.tiem}>
+            <li className={scss.item_order}>
                 <Link to="sell" className={scss.link}>sell</Link>          
             </li>
         </ul>
