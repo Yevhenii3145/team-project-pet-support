860840c8ab1f9f37efc9f4cb956c5a2221b9728d
Bykova Kateryna <96261573+Bykova-Kateryna@users.noreply.github.com>
2023-02-03 10:57:37 +0200

    Update AddsPetContent.jsx

--- src/components/ModalAddsPet/ModalAddPetPages/AddsPetContent.jsx
+++ src/components/ModalAddsPet/ModalAddPetPages/AddsPetContent.jsx
@@ -1,19 +1,19 @@
 import { useState } from 'react';
 import scss from './modal-add-pet-pages.module.scss';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+// import { Formik, Form, Field, ErrorMessage } from 'formik';
+// import * as Yup from 'yup';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const schemasForStepFirst = Yup.object().shape({
-  name: Yup.string(),
-  date: Yup.date(),
-  bird: Yup.string(),
-});
+// const schemasForStepFirst = Yup.object().shape({
+//   name: Yup.string(),
+//   date: Yup.date(),
+//   bird: Yup.string(),
+// });
 
-const schemasForStepSecond = Yup.object().shape({
-  image: Yup.string().required(),
-  comments: Yup.string().required(),
-});
+// const schemasForStepSecond = Yup.object().shape({
+//   image: Yup.string().required(),
+//   comments: Yup.string().required(),
+// });
 
 const AddsPetContent = ({ close }) => {
   // const [modalActive, setModalActive] = useState(true);
@@ -23,37 +23,72 @@ const AddsPetContent = ({ close }) => {
 //   console.log(imageURL)
 
 
-  const initialValue = {
-    name: '',
-    date: '',
-    bird: '',
-    image: '',
-    comments: '',
-  };
+  // const initialValue = {
+  //   name: '',
+  //   date: '',
+  //   bird: '',
+  //   image: '',
+  //   comments: '',
+  // };
 
   const changeStep = () => {
-    return setStepOne(true);
+    return setStepOne(!stepOne);
   };
 
-  const handleSubmit = (values, actions) => {
-    if (stepOne) {
-      setStepOne(false);
-      setPet(values);
-      return console.log(values);
+  const handleSubmitForStepOne = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const {name, date, bird} = form.elements;
+    const dataFirst = {
+      name: name.value,
+      date: date.value,
+      bird: bird.value,
     }
-    if (!stepOne) {
-      const petInfo = {
-        name: pet.name,
-        date: pet.date,
-        bird: pet.bird,
-        image: values.image,
-        comments: values.comments,
-      };
-      console.log(petInfo);
-      actions.resetForm();
-      return setStepOne(true);
-    }
-  };
+    setPet(dataFirst)
+    console.log(dataFirst)
+    return changeStep()
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const form = e.currentTarget;
+    const {image, comments} = form.elements;
+    // const {name, date, bird, image, comments} = form.elements;
+    // const login = form.elements.login.value;
+
+    // console.log(title.value);
+    // console.log(author.value);
+    // console.log(cover.files);
+
+    const data = new FormData();
+    data.append("name", pet.name);
+    data.append("date", pet.date);
+    data.append("bird", pet.bird);
+    data.append("comments", comments.value);
+    data.append("image", image.files[0]);
+    console.log(pet.name, pet.date, pet.bird, comments.value, image.files[0])
+    return form.reset()
+  }
+
+  // const handleSubmit = (values, actions) => {
+  //   if (stepOne) {
+  //     setStepOne(false);
+  //     setPet(values);
+  //     return console.log(values);
+  //   }
+  //   if (!stepOne) {
+  //     const petInfo = {
+  //       name: pet.name,
+  //       date: pet.date,
+  //       bird: pet.bird,
+  //       image: values.image,
+  //       comments: values.comments,
+  //     };
+  //     console.log(petInfo);
+  //     actions.resetForm();
+  //     return setStepOne(true);
+  //   }
+  // };
 
 //   const handleImageChange = (e) => {
 //     console.log(e.target.files[0])
@@ -73,8 +108,43 @@ const AddsPetContent = ({ close }) => {
   return (
     <div className={scss.modalAdds_page}>
       <h3 className={scss.modalAdds_page__tittle}>Add pet</h3>
+        {stepOne &&
+        <form onSubmit={handleSubmitForStepOne}>
+        <label className={`${scss.modalAdds_page__label} ${scss.modalAdds_page_box}`}>Name pet</label>
+        <input className={scss.modalAdds_page__input} name="name" placeholder="Type name pet" type="text" required/>
+        <label className={`${scss.modalAdds_page__label} ${scss.modalAdds_page_box}`}>Data of birth</label>
+        <input className={scss.modalAdds_page__input} name="date" type="text" placeholder="Type date of birth" required/>
+        <label className={`${scss.modalAdds_page__label} ${scss.modalAdds_page_box}`}>Breed</label>
+        <input className={scss.modalAdds_page__input} type="text" name="bird" placeholder="Type bird" required/>
+        <div className={scss.addPet__button}>
+        <button className={`${scss.button__primary_main} ${scss.modalAdds_page__button}`} type="submit">Next</button>
+        <span className={`${scss.button__primary_not_main} ${scss.modalAdds_page__button}`} onClick={close}>Cansel</span>
+        </div>
+        </form>
+        }
+        {!stepOne &&
+        <form action="" id="book-add-form" encType="multipart/form-data" onSubmit={handleSubmit}>
+                <div className={scss.add__pet__container}>
+                <p className={scss.modalAdds_page__field}>Add photo and some comments</p>
+              
+                <input className={scss.addspet__imgInput} type="file" name="image" accept="image/png, image/jpeg, image/jpg, image/webp" id="img" required multiple/>
+                <label className={scss.addspet__imgLabel} htmlFor="img"></label>
+                
+              
+                <label className={`${scss.modalAdds_page__label} ${scss.modalAdds_commit_box}`}>Comments</label>
+                <textarea className={scss.modalAdds_commit} type="text" name="comments" placeholder="Type comments" required/>
+                
+                <div className={scss.addPet__button}>
+                <button className={`${scss.button__primary_main} ${scss.modalAdds_page__button}`} type="submit">Done</button>
+                <span className={`${scss.button__primary_not_main} ${scss.modalAdds_page__button}`} onClick={changeStep}>Back</span>
+                </div>
+                </div>
+          </form>
+        }
+
+
       {/* <img src={`${imageURL}`} alt="pet" /> */}
-      {stepOne && (
+      {/* {stepOne && (
         <>
           <Formik
             validationSchema={schemasForStepFirst}
@@ -128,19 +198,7 @@ const AddsPetContent = ({ close }) => {
                 render={msg => Notify.warning(`${msg}`)}
               />
               <div className={scss.addPet__button}>
-                <button
-                  className={`${scss.button__primary_main} ${scss.modalAdds_page__button}`}
-                  type="submit"
-                >
-                  Next
-                </button>
-                <span
-                  className={`${scss.button__primary_not_main} ${scss.modalAdds_page__button}`}
-                  type="button"
-                  onClick={close}
-                >
-                  Cansel
-                </span>
+
               </div>
             </Form>
           </Formik>
@@ -199,7 +257,7 @@ const AddsPetContent = ({ close }) => {
             </Form>
           </Formik>
         </>
-      )}
+      )} */}
 
       {/* <form>
                 <div className={scss.modalAdds_page_box}>
