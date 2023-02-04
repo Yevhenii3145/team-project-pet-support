f2eb23bd27d60586b0ef2440c70c9762c1d6bcc2
gdutchak <galyna.dutchak99@gmail.com>
2023-02-04 21:32:12 +0200

    add link

--- src/components/Nav/GoogleNav/GoogleNav.jsx
+++ src/components/Nav/GoogleNav/GoogleNav.jsx
@@ -4,6 +4,7 @@ import scss from "./google-nav.module.scss";
 import { useSelector, useDispatch } from "react-redux";
 import { setMenuActive } from "redux/menuSlice";
 
+const {REACT_APP_BASE_URL} = process.env;
 
 const GoogleAuth = () => {
 
@@ -13,14 +14,14 @@ const GoogleAuth = () => {
     return (
         <>
         <p className={scss.googleDecs}>Or sign in with</p>
-        {isActive === true ? <NavLink to={'user'} className={scss.googleLink} onClick={()=>dispatch(setMenuActive(!isActive))}>
+        {isActive === true ? <a href={`${REACT_APP_BASE_URL}/auth/google`} className={scss.googleLink} onClick={()=>dispatch(setMenuActive(!isActive))}>
         <SvgInsert id="icon-google"/>
         <p className={scss.googleDecs}>Google</p>
-        </NavLink>
-        : <NavLink to={'user'} className={scss.googleLink}>
+        </a>
+        : <a href={`${REACT_APP_BASE_URL}/auth/google`} className={scss.googleLink}>
         <SvgInsert id="icon-google"/>
         <p className={scss.googleDecs}>Google</p>
-        </NavLink>}
+        </a>}
         </>
     )
 }
