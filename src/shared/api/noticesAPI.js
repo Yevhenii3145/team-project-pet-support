f8c02106d0fbf2a6895be9c598816247c99fea68
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2023-02-06 17:03:37 +0200

    исправил пути редакс

--- src/shared/api/noticesAPI.js
+++ src/shared/api/noticesAPI.js
@@ -1,25 +1,27 @@
 import axios from 'axios';
 
+const { REACT_APP_BASE_URL } = process.env;
+
 const instance = axios.create({
-    baseURL: 'http://localhost:4001/api'
-})
+  baseURL: `${REACT_APP_BASE_URL}/api`,
+});
 
-export const getCategoryNotices = async (categoryName) => {
-    const data = await instance.get(`/notices/${categoryName}`);
-    return data;
-}
+export const getCategoryNotices = async categoryName => {
+  const data = await instance.get(`/notices/${categoryName}`);
+  return data;
+};
 
-export const addNoticeToFavorite = async (noticeId) => {
-    const data = await instance.patch(`/notices/favorites/${noticeId}`);
-    return data;
-}
+export const addNoticeToFavorite = async noticeId => {
+  const data = await instance.patch(`/notices/favorites/${noticeId}`);
+  return data;
+};
 
-export const deleteNotice = async (noticeId) => {
-    const data = await instance.delete(`/notices/${noticeId}`);
-    return data;
-}
+export const deleteNotice = async noticeId => {
+  const data = await instance.delete(`/notices/${noticeId}`);
+  return data;
+};
 
-export const searchNotice = async (noticeId) => {
-    const data = await instance.get(`/notices/notice/${noticeId}`);
-    return data;
-}
+export const searchNotice = async noticeId => {
+  const data = await instance.get(`/notices/notice/${noticeId}`);
+  return data;
+};
