3110a5832ccedeee5554931e265d0c5c51097004
Dmitriy Zaiets <95711961+demkona@users.noreply.github.com>
2023-02-07 23:24:29 +0200

    add validation

--- src/components/AddNoticeButton/AddNoticeButton.jsx
+++ src/components/AddNoticeButton/AddNoticeButton.jsx
@@ -1,12 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
-import ModalAddNotice from 'components/ModalAddNotice/ModalAddNotice';
-import SvgInsert from 'components/Svg/Svg';
+import { formatDistanceStrict } from 'date-fns';
+import { Notify } from 'notiflix';
 import scss from './add-notice-button.module.scss';
+import SvgInsert from 'components/Svg/Svg';
+import ModalAddNotice from 'components/ModalAddNotice/ModalAddNotice';
+import useAuth from 'shared/hooks/useAuth';
+import { getUserId } from 'redux/selectors';
 import AddsPetContent from 'components/ModalAddNotice/ModalAddNoticeForm/AddsNoticeContent';
 
 const AddNoticeButton = () => {
   const [modalShow, setModalShow] = useState(false);
-
+// const isLogin = useAuth();
+// const dispach = useDispatch;
+// const userId = useSelector(getUserId)
+// const addNotice = (noticeId) => {
+//   if(isLogin) {
+//     return dispatchEvent(addNotice(noticeId));
+//   }
+//   Notify.failure("You need authorization")
+// }
   const closeModal = () => {
     setModalShow(false);
   };
@@ -22,7 +35,7 @@ const AddNoticeButton = () => {
         type="button"
         onClick={showModal}
       >
-        <SvgInsert id="icon-add-pet" />
+        <SvgInsert id="icon-add-notice" />
         Add pet
       </button>
       {modalShow && (
