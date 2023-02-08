7390a4fa5644038629a3153e2730ec6171afb6b4
Elena-Holz <97180066+Elena-Holz@users.noreply.github.com>
2023-02-08 17:37:09 +0200

    UserPet8.02

--- src/components/PetsList/PetsList.jsx
+++ src/components/PetsList/PetsList.jsx
@@ -2,37 +2,26 @@ import scss from "./pets-list.module.scss";
 import SvgInsert from "../Svg/Svg";
 import {getPets} from "../../redux/selectors"
 import { useSelector, useDispatch } from "react-redux";
-// import {  useEffect } from 'react';
+import {  useEffect } from 'react';
 import operations from '../../redux/operations'; 
 
 
-export function PetsList(userId) {
+export function PetsList() {
      
-  // const { pets } = useSelector(getPets());
+  const pets = useSelector(getPets);
+  console.log(pets)
 
   
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
     
-//   useEffect(() => {
-//     dispatch(operations.getUserPet(pets))
-//     }, [dispatch]);
-//     console.log(pets)
-    
-    const pets = [{
+ 
+        const onDeletePet= (petId) => {
+       const action = operations.deletePet(petId);
+        dispatch(action);
+}
 
-        name: 'BFGHJ',
-        date: '12.12.2022',
-        breed: 'fghj',
-        comments: 'dsfghjkl.',
-    }]
-
-//         const onDeletePet= (petId) => {
-//        const action = operations.deletePet(petId);
-//         dispatch(action);
-// }
-
-    const elements = pets.map(({ name, date, breed, image, comments, userId }) => {
-        return <li className={scss.petsList_box} key={userId}>
+    const elements = pets.map(({ name, date, breed, image, comments, petId }) => {
+        return <li className={scss.petsList_box} key={petId}>
             <img className={scss.petsList_photo} src={image} alt="" />
                     <div className={scss.petsList_text_box}>
                         <p className={scss.petsList_field}>Name:  {name}</p>
