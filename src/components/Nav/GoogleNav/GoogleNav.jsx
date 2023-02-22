99fd20621a2a4b97b6f9a74459c503a2c4999a54
Bykova Kateryna <96261573+Bykova-Kateryna@users.noreply.github.com>
2023-02-22 22:14:44 +0200

    registration page
    додала фейсбук
    пофіксила реєстрацію

--- src/components/Nav/GoogleNav/GoogleNav.jsx
+++ src/components/Nav/GoogleNav/GoogleNav.jsx
@@ -1,28 +1,57 @@
-import SvgInsert from "../../Svg/Svg";
-import scss from "./google-nav.module.scss";
-import { useSelector, useDispatch } from "react-redux";
-import { setMenuActive } from "redux/menuSlice";
+import SvgInsert from '../../Svg/Svg';
+import scss from './google-nav.module.scss';
+import { useSelector, useDispatch } from 'react-redux';
+import { setMenuActive } from 'redux/menuSlice';
 
-const {REACT_APP_BASE_URL} = process.env;
+const { REACT_APP_BASE_URL } = process.env;
 
 const GoogleAuth = () => {
+  const isActive = useSelector(state => state.menu.menuActive);
+  const dispatch = useDispatch();
 
-    const isActive = useSelector(state=>state.menu.menuActive)
-    const dispatch = useDispatch();
-
-    return (
-        <div className={scss.googleBox}>
-        <p className={scss.googleDecs}>Or sign in with</p>
-        {isActive === true ? <a href={`${REACT_APP_BASE_URL}/api/auth/google`} className={scss.googleLink} onClick={()=>dispatch(setMenuActive(!isActive))}>
-        <SvgInsert id="icon-google"/>
-        <p className={scss.googleDecs}>Google</p>
-        </a>
-        : <a href={`${REACT_APP_BASE_URL}/api/auth/google`} className={scss.googleLink}>
-        <SvgInsert id="icon-google"/>
-        <p className={scss.googleDecs}>Google</p>
-        </a>}
-        </div>
-    )
-}
+  return (
+    <div className={scss.googleBox}>
+      <p className={scss.googleDecs}>Or sign in with</p>
+      {isActive === true ? (
+        <>
+          <a
+            href={`${REACT_APP_BASE_URL}/api/auth/google`}
+            className={scss.googleLink}
+            onClick={() => dispatch(setMenuActive(!isActive))}
+          >
+            <SvgInsert id="icon-google" />
+            <p className={scss.googleDecs}>Google</p>
+          </a>
+          <a
+            href={`${REACT_APP_BASE_URL}/api/auth/facebook`}
+            className={scss.googleLink}
+            onClick={() => dispatch(setMenuActive(!isActive))}
+          >
+            <SvgInsert id="facebook" />
+            <p className={scss.googleDecs}>Facebook</p>
+          </a>
+        </>
+      ) : (
+        <>
+          <a
+            href={`${REACT_APP_BASE_URL}/api/auth/google`}
+            className={scss.googleLink}
+          >
+            <SvgInsert id="icon-google" />
+            <p className={scss.googleDecs}>Google</p>
+          </a>
+          <a
+            href={`${REACT_APP_BASE_URL}/api/auth/facebook`}
+            className={scss.googleLink}
+            onClick={() => dispatch(setMenuActive(!isActive))}
+          >
+            <SvgInsert id="facebook" />
+            <p className={scss.googleDecs}>Facebook</p>
+          </a>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default GoogleAuth;
